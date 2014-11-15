64472edb8be9124cb5cc0bdde15f22a215bc9698
ashenoi <ashshenoi8@gmail.com>
2014-11-15 12:32:23 -0500

    Adding master.sh for running all the commands

---
+++ test/master.sh
@@ -0,0 +1,18 @@
+make compile
+make sim IMAGE=Dimetrodon 
+make sim IMAGE=Grove2 
+make sim IMAGE=Grove3 
+make sim IMAGE=Hydrangea 
+make sim IMAGE=RubberWhale 
+make sim IMAGE=Urban2 
+make sim IMAGE=Urban3 
+make sim IMAGE=Venus
+make compile_GT
+make extractflow IMAGE=Dimetrodon 
+make extractflow IMAGE=Grove2 
+make extractflow IMAGE=Grove3 
+make extractflow IMAGE=Hydrangea 
+make extractflow IMAGE=RubberWhale 
+make extractflow IMAGE=Urban2 
+make extractflow IMAGE=Urban3 
+make extractflow IMAGE=Venus
