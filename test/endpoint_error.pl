9c5784327059a4747be3aa41308f460c240d69b3
ashenoi <ashshenoi8@gmail.com>
2014-11-15 12:48:17 -0500

    Adding perl script for endpoint error calculation

---
+++ test/endpoint_error.pl
@@ -0,0 +1,20 @@
+open(FILE1,"<$ARGV[0]") or die("Could not open");
+open(FILE2,"<$ARGV[1]") or die("Could not open");
+
+$count=0;
+$EE=0;
+while($line1=<FILE1>)
+{
+	$line2 = <FILE2>;
+	chomp($line1);
+	chomp($line2);
+	@mv1 = split(',',$line1);
+	@mv2 = split(',',$line2);
+	$EE=(($mv1[0]-$mv2[0])**2+($mv1[1]-$mv2[1])**2)**0.5;
+	$count+=1;
+}
+print $EE,"\n";
+$EE=$EE/$count;
+print $EE,"\n";
+close(FILE1);
+close(FILE2);
